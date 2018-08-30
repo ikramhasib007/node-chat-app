ec57b290c5b5d63a2e55ac9b934769ea02a77b5e
ikramhasib007 <ikramhasib007@gmail.com>
2018-08-30 21:50:10 +0600

    generate the message and tests

---
+++ server/utils/message.test.js
@@ -0,0 +1,14 @@
+const expect = require('expect');
+
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+    it('should generate correct message object', () => {
+        var from = 'john';
+        var text = 'welcome';
+        var message = generateMessage(from, text);
+        expect(message.from).toBe(from);
+        expect(message.text).toBe(text);
+        // expect(message.createdAt).toBeA('number');
+    });
+});
